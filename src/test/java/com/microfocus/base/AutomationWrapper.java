58febe8da827df541699413221bf2aedf24060cd
Balaji <balaji0017@gmail.com>
2023-09-07 16:38:18 +0530

    frameworkcode-added comments

--- src/test/java/com/microfocus/base/AutomationWrapper.java
+++ src/test/java/com/microfocus/base/AutomationWrapper.java
@@ -10,7 +10,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-
+/**
+ * This class contains browser configuration
+ */
 public class AutomationWrapper {
 
 	protected WebDriver driver;
